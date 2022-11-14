f950d74083222847954edf4f5ed88d2c6b97651e
ihebabbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-11-15 00:59:49 +0100

    remove spaces

--- Solutions/P1.java
+++ Solutions/P1.java
@@ -4,7 +4,9 @@ public class P1 {
         System.out.println(twoSum(nums, 17));
     }
 
-    public static boolean twoSum(int[] nums, int k) {
+    public static boolean
+
+        twoSum(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == k) {
@@ -14,10 +16,4 @@ public class P1 {
         }
         return false;
     }
-
-
-
-
-
-
 }
