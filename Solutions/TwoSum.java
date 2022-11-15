46f31fa45455f54ac42f26244b73135a25ed8182
ihebabbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-11-15 01:25:36 +0100

    quick update

--- Solutions/TwoSum.java
+++ Solutions/TwoSum.java
@@ -4,9 +4,7 @@ public class TwoSum {
         System.out.println(twoSum(nums, 17));
     }
 
-    public static boolean
-
-        twoSum(int[] nums, int k) {
+    public static boolean twoSum(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == k) {
